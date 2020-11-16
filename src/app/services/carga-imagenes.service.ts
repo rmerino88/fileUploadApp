06ae1408ebe72b35370689fa7bc66b818ee37e3c
Raúl Merino <rmerino_88@hotmail.com>
2020-11-16 20:22:17 +0100

    Correccion obtener url del elemento subido

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -34,13 +34,20 @@ export class CargaImagenesService {
         storageRef.child(`${ this.CARPETA_IMAGENES }/${ item.nombreArchivo}`).put( item.archivo );
 
       uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
-        ( snapshot: firebase.storage.UploadTaskSnapshot ) => item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100,
+        ( snapshot: firebase.storage.UploadTaskSnapshot ) => {
+          item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
+        },
         ( error ) => console.error('Error al subir archivo', error),
         () => {
           console.log('Imagen cargada correctamente');
-          item.url = uploadTask.snapshot.downloadURL;
-          item.estaSubiendo = false;
-          this.guardarImagen( { nombre: item.nombreArchivo, url: item.url } );
+          // item.url = uploadTask.snapshot.downloadURL;
+          // item.url = uploadTask.snapshot.storageReference?.downloadURL;
+          storageRef.child(`${ this.CARPETA_IMAGENES }/${ item.nombreArchivo}`).getDownloadURL().then( element => {
+              item.url = element;
+              console.log('URL : ' + item.url);
+              item.estaSubiendo = false;
+              this.guardarImagen( { nombre: item.nombreArchivo, url: item.url } );
+            });
         }
       );
     }
